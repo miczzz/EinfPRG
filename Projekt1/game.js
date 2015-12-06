6d014dc65ead7468ca6c463215d932fb175d4652
miczzz <mziesemann@gmail.com>
2015-12-06 14:37:29 +0100

    Resetbutton
    Startbutton zum Start- und Resetbutton ausgebaut

--- Projekt1/game.js
+++ Projekt1/game.js
@@ -70,13 +70,26 @@ function mouse(e)		// Koordinaten der Maus
 
 function start()
 {
+  //Für Reset
+  document.getElementById("startButton").innerHTML= "Reset"; // Startbutton -> Resetbutton
+  schluessel_ctx.clearRect(0,0,70,63); // Schlüsselgrafiken reseten
+  schluessel2_ctx.clearRect(0,0,70,63);
+  textfeld_ctx.clearRect(0, 0, 800, 75); // Text vom Textfeld löschen
+  //Variablen auf Default-Werte zurücksetzen
+  currentClickZone = "nowhere";
+  hasKey = 0; // hat man den Schlüssel gefunden? 0 = nein, 1 = ja
+  hasRightKey = 0; // hat man den Türschlüssel gefunden?
+  // Reset Ende  
+  
   background_ctx.drawImage(hintergrund, 0, 0);	// Hintergrundbild
   inventar_ctx.drawImage(inventarhg, 0,0);		// Inventarbild
   textfeld_ctx.fillText("Du wachst alleine in diesem Raum auf. Die T\xFCr ist verschlossen. Finde einen Weg hinaus.", 25, 25);
-	for (var i = 0; i < clickZones.length; i += 1) {
+
+  for (var i = 0; i < clickZones.length; i += 1) {
 		clickZones[i].addEventListener("click", click);
 		clickZones[i].style.visibility = "visible";
 	}
+
 	/*
 	Test, so lässt sich ein CSS-Element direkt aufrufen und ändern:
 	document.querySelector("header").style.color = "#5aF4F6";	
@@ -90,31 +103,25 @@ function changeText()
  	if(currentClickZone == "Fenster"){
 	textfeld_ctx.fillText("Du versuchst das Fenster zu \xF6ffnen, es ist aber fest verriegelt.", 25, 25);
 	textfeld_ctx.fillText("So kommst du hier nicht raus.", 25, 45);
-	} 
-	
+	} 	
 	if(currentClickZone == "Pflanze"){
 	textfeld_ctx.fillText("Eine Pflanze, die offensichtlich genug Wasser erh\xE4lt und gedeiht.", 25, 25);
-	} 
-	
+	} 	
 	if(currentClickZone == "Pflanzentopf"){
 	textfeld_ctx.fillText("Du gr\xE4bst in der Erde (ja, so verzweifelt bist du), kannst aber nichts finden.", 25, 25);
 	} 
-
  	if(currentClickZone == "Bild"){
 	textfeld_ctx.fillText("Du schaust dir das Bild an. Aus irgendeinem Grund gef\xE4llt dir das Bild nicht, du kannst", 25, 25);
 	textfeld_ctx.fillText("aber nicht sagen warum. Ansonsten entdeckst du nichts ungew\xF6hnliches.", 25, 45);
 	textfeld_ctx.fillText("Test dritte Zeile gut lesbar? Ja, kann man sagen.", 25, 65);
-	} 
-	
+	} 	
 	if(currentClickZone == "Obstschale"){
 	textfeld_ctx.fillText("Platzhalter Obstschale", 25, 25);
-	} 
-	
+	} 	
 	if(currentClickZone == "Teppich"){
 	textfeld_ctx.fillText("Du hebst den Teppich an um darunter zu schauen, das ist immerhin ein h\xE4ufiges", 25, 25);
 	textfeld_ctx.fillText("Versteck von Schl\xFCsseln. Au\xDFer Staub findest du hier aber nichts.", 25, 45);
 	} 
-
 	if(currentClickZone == "KommodeLinks"){
 		if(hasKey==0){
 	textfeld_ctx.fillText("Du \xF6ffnest die Schubladen...und du findest einen Schl\xFCssel!", 25, 25);
@@ -124,8 +131,7 @@ function changeText()
 		else if(hasKey==1){
 		textfeld_ctx.fillText("Au\xDFer dem Schl\xFCssel ist hier nichts besonderes...und den hast du schon aufgehoben!", 25, 25);
 		}
-	} 
-	
+	} 	
 	if(currentClickZone == "Regal1"){
 	textfeld_ctx.fillText("Platzhalter Regal1", 25, 25);
 	}
@@ -161,9 +167,7 @@ function changeText()
 	}
 		if(currentClickZone == "Regal12"){
 	textfeld_ctx.fillText("Platzhalter Regal12", 25, 25);
-	}
-	
-	
+	}	
 	
 	if(currentClickZone == "Inventar1"){
 		if(hasKey==0){
@@ -172,8 +176,7 @@ function changeText()
 		if(hasKey==1){
 		textfeld_ctx.fillText("Ja! Du hast den Schl\xFCssel gefunden!", 25, 25);
 		}
-	} 	
-	
+	} 		
 	if(currentClickZone == "Inventar2"){
 		if(hasRightKey==0){
 		textfeld_ctx.fillText("Dein Inventar ist leer.", 25, 25);
@@ -182,7 +185,7 @@ function changeText()
 		textfeld_ctx.fillText("Ein zweiter Schl\xFCssel, der dich hoffentlich hier rausf\xFChrt!", 25, 25);
 		}
 	} 	
-
+	
 	if(currentClickZone == "Tuer"){
 		if(hasKey==0){
 		textfeld_ctx.fillText("Die T\xFCr ist abgeschlossen!", 25, 25);
@@ -193,8 +196,7 @@ function changeText()
 		if(hasRightKey==1){
 		textfeld_ctx.fillText("Der Schl\xFCssel passt! Du kannst dein Gl\xFCck kaum fassen und genie\xDFt die Freiheit!", 25, 25);
 		}
-	}
-	
+	}	
 	if(currentClickZone == "KommodeRechts"){
 	if(hasKey==0){
 	textfeld_ctx.fillText("Die verdammte Kommode ist abgeschlossen!", 25, 25);
